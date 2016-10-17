63660518849c37272fab23e79cb2d1b1e3497535
Malcolm <Malcolm259@gmail.com>
2016-10-17 17:30:17 -0400

    dqwdq

--- president/president.py
+++ president/president.py
@@ -59,26 +59,26 @@ class president:
         
     # COMMANDS END HERE
 
-    # COG DEFINITIONS, DO NOT CHANGE ME
-    def presidentclear(self, settings):
-    	dataIO.save_json(self.file_path, self.system)
+# COG DEFINITIONS, DO NOT CHANGE ME
+def presidentclear(self, settings):
+	dataIO.save_json(self.file_path, self.system)
 
-    # Checks the server for specific settings
-    # if the settings don't exist, create them
-    def check_server_settings(self, server):
-        if server.id not in self.system["Servers"]:
-            self.system["Servers"][server.id] = {"President": {},
-                                                 "Config": {"Current President": "None", "Election Underway": "No",
-                                                            "Cooldown": False, "Time Remaining": 0,
-                                                            "Default Cooldown": 0},
-                                                 }
-            dataIO.save_json(self.file_path, self.system)
-            print("Creating default config for President on server: {}".format(server.name))
-            path = self.system["Servers"][server.id]
-            return path
-        else:
-            path = self.system["Servers"][server.id]
-            return path
+# Checks the server for specific settings
+# if the settings don't exist, create them
+def check_server_settings(self, server):
+    if server.id not in self.system["Servers"]:
+        self.system["Servers"][server.id] = {"President": {},
+                                             "Config": {"Current President": "None", "Election Underway": "No",
+                                                        "Cooldown": False, "Time Remaining": 0,
+                                                        "Default Cooldown": 0},
+                                             }
+        dataIO.save_json(self.file_path, self.system)
+        print("Creating default config for President on server: {}".format(server.name))
+        path = self.system["Servers"][server.id]
+        return path
+    else:
+        path = self.system["Servers"][server.id]
+        return path
 
 
 def check_folders():
