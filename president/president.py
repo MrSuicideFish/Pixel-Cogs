cad91fbcefd204eddd7df952c62fb7546ffccd17
Malcolm <Malcolm259@gmail.com>
2016-10-17 17:21:01 -0400

    refixed

--- president/president.py
+++ president/president.py
@@ -17,37 +17,38 @@ try:   # Check if Tabulate is installed
 except:
     tabulateAvailable = False
 
+
 class president:
     """Run for Prez of your discord server"""
 
-	#On script initiaion we do this first
+    # On script initiaion we do this first
     def __init__(self, bot):
         self.bot = bot
         self.file_path = "data/president/system.json"
         self.system = dataIO.load_json(self.file_path)
 
-	#COMMANDS START HERE
+        # COMMANDS START HERE
 
     #--define the president command
     @commands.group(pass_context=True, no_pm=True)
-    async def president(self,ctx):
+    async def president(self, ctx):
         """General president related commands"""
 
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
     #--Create the 'nominate' command
-    #we write pass_context so that discord will send us info about the server that we can use
-    @president.command( name="nominate", pass_context=True )
-    async def _nominate_president(self,ctx):
+    # we write pass_context so that discord will send us info about the server
+    # that we can use
+    @president.command(name="nominate", pass_context=True)
+    async def _nominate_president(self, ctx):
         user = ctx.message.author
         server = ctx.message.server
         settings = self.check_server_settings(server)
-        #output a message from the bot
+        # output a message from the bot
         await self.bot.say("I can do stuff!")
 
-
-    #DEBUGGING
+    # DEBUGGING
     @president.command(name="reset", pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
     async def _reset_president(self, ctx):
@@ -56,46 +57,50 @@ class president:
         self.presidentclear(settings)
         await self.bot.say("President has been reset.")
 
-	#COMMANDS END HERE
+        # COMMANDS END HERE
 
-	#COG DEFINITIONS, DO NOT CHANGE ME
-	def presidentclear(self, settings):
-	    dataIO.save_json(self.file_path, self.system)
+        # COG DEFINITIONS, DO NOT CHANGE ME
+        def presidentclear(self, settings):
+        dataIO.save_json(self.file_path, self.system)
 
-	#Checks the server for specific settings
-	#if the settings don't exist, create them
-	def check_server_settings(self,server):
-		if server.id not in self.system["Servers"]:
-			self.system["Servers"][server.id] = {"President": {},
-												 "Config": { "Current President": "None",
-															"Election Underway" : "No",
-															"Cooldown"          : False, 
-															"Time Remaining"    : 0,
-															"Default Cooldown"  : 0 },
-												}
-			dataIO.save_json(self.file_path, self.system)
-			print("Creating default config for President on server: {}".format(server.name))
-			path = self.system["Servers"][server.id]
-			return path
-		else:
-			path = self.system["Servers"][server.id]
-			return path
-			
+        # Checks the server for specific settings
+        # if the settings don't exist, create them
+        def check_server_settings(self, server):
+            if server.id not in self.system["Servers"]:
+                self.system["Servers"][server.id] = {"President": {},
+                                                     "Config": {"Current President": "None",
+                                                                "Election Underway": "No",
+                                                                "Cooldown": False,
+                                                                "Time Remaining": 0,
+                                                                "Default Cooldown": 0},
+                                                     }
+                dataIO.save_json(self.file_path, self.system)
+                print("Creating default config for President on server: {}".format(
+                    server.name))
+                path = self.system["Servers"][server.id]
+                return path
+            else:
+                path = self.system["Servers"][server.id]
+                return path
+
+
 def check_folders():
-	if not os.path.exists("data/president"):
-		print("Creating president folder...")
-		os.makedirs("data/president")
+    if not os.path.exists("data/president"):
+        print("Creating president folder...")
+        os.makedirs("data/president")
+
 
 def check_files():
-	default = {"Servers": {}
-			   }
-	f = "data/president/system.json"
-	if not dataIO.is_valid_json(f):
-		print("Making president .json...")
-		dataIO.save_json(f, default)
-			
+    default = {"Servers": {}
+               }
+    f = "data/president/system.json"
+    if not dataIO.is_valid_json(f):
+        print("Making president .json...")
+        dataIO.save_json(f, default)
+
+
 def setup(bot):
-	check_folders()
-	check_files()
-	n = president(bot)
-	bot.add_cog(n)
+    check_folders()
+    check_files()
+    n = president(bot)
+    bot.add_cog(n)
