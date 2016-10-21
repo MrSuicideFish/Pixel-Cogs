a5d057fe199cf26701c541919405a035fd52274a
MrSuicide <Malcolm259@gmail.com>
2016-10-20 21:15:47 -0400

    dwqdqwd

--- president/president.py
+++ president/president.py
@@ -51,15 +51,15 @@ class president:
 
         if len(ctx.message.mentions) < 1:
             if nominatedUser is not "" or None:
-                # nominatedMember = server.get_member(nominatedUser)
-                await self.bot.say(nominatedUser)
+                nominatedMember = server.get_member_by_name(nominatedUser)
                 # self.candidates_add(nominatedMember.id, nominatedMember.nick, settings)
+                await self.bot.say(nominatedUser)
             else:
                 await self.bot.say("Missing name.")
         else:
             nominatedMember = server.get_member(ctx.message.mentions[0]);
-            await self.bot.say(ctx.message.mentions[0])
             # self.candidates_add(nominatedMember.id, nominatedMember.nick, settings)
+            await self.bot.say(ctx.message.mentions[0].id)
 
                 # DEBUGGING
     @president.command(name="reset", pass_context=True)
